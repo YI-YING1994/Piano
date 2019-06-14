fa768da71db85949193161957f9fadebef96b1be
YI-YING1994 <01360912@me.mcu.edu.tw>
2019-06-14 09:30:19 +0800

    Hover on button feature complete!

--- Piano/Piano/ViewController.swift
+++ Piano/Piano/ViewController.swift
@@ -10,35 +10,96 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    var sounds: [String: AVAudioPlayer?]!
+    
+    // 載入音效
+    let sounds = [try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C3", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "D", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "E", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "F", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "G", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "A", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "B", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C4", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C#", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "D#", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "F#", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "G#", withExtension: "mp3")!),
+                  try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "A#", withExtension: "mp3")!)]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // 載入音效
-        sounds = ["A": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "A", withExtension: "mp3")!),
-                  "C3": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C3", withExtension: "mp3")!),
-                  "B": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "B", withExtension: "mp3")!),
-                  "C#": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C#", withExtension: "mp3")!),
-                  "C4": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "C4", withExtension: "mp3")!),
-                  "G": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "G", withExtension: "mp3")!),
-                  "F": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "F", withExtension: "mp3")!),
-                  "D": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "D", withExtension: "mp3")!),
-                  "E": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "E", withExtension: "mp3")!),
-                  "D#": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "D#", withExtension: "mp3")!),
-                  "F#": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "F#", withExtension: "mp3")!),
-                  "G#": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "G#", withExtension: "mp3")!),
-                  "A#": try? AVAudioPlayer(contentsOf: Bundle.main.url(forResource: "A#", withExtension: "mp3")!)]
+        
+        // Play even in silent mode
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(.playback, mode: .default, options: [])
+        }
+        catch {
+            print(error)
+        }
     }
-
+    
+    var currentTouch: UIButton!
+    @IBOutlet var buttons: [UIButton]!
+    @IBAction func hoverOnButton(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .changed {
+            let point = sender.location(in: self.view)
+            var temp: UIButton?
+            for button in buttons {
+                if point.x >= button.frame.minX &&
+                    point.x <= button.frame.maxX &&
+                    point.y >= button.frame.minY &&
+                    point.y <= button.frame.maxY &&
+                    temp?.tag ?? 0 <= button.tag {
+                    temp = button
+                }
+            }
+            if currentTouch != temp && temp != nil {
+                if currentTouch.subviews.count > 1 {
+                    currentTouch.subviews.last?.removeFromSuperview()
+                }
+                currentTouch = temp
+                let view = UIView(frame: CGRect(x: 0, y: 0, width: currentTouch.frame.width, height: currentTouch.frame.height))
+                view.backgroundColor = #colorLiteral(red: 1, green: 0.6597909764, blue: 1, alpha: 0.5)
+                currentTouch.addSubview(view)
+                currentTouch?.sendActions(for: .touchDown)
+                
+            }
+        }
+        if sender.state == .ended {
+            if currentTouch.subviews.count > 1 {
+                currentTouch.subviews.last?.removeFromSuperview()
+            }
+        }
+    }
+    
+    
     // 播放音效
     @IBAction func playSound(_ sender: UIButton) {
+        currentTouch = sender
+        
         // 選擇要播放哪個音效
-        let player = sounds[sender.currentTitle!]!
-
+        let player = sounds[sender.tag]!
+    
         // 將播放時間歸 0
-        player?.currentTime = 0
-        player?.play()
+        player.currentTime = 0
+        player.play()
     }
-
+    
 }
 
+extension UIButton {
+    open override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted && isHighlighted != oldValue {
+                let view = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+                view.backgroundColor = #colorLiteral(red: 1, green: 0.6597909764, blue: 1, alpha: 0.5)
+                self.addSubview(view)
+            }
+
+            if !isHighlighted && self.subviews.count > 1 {
+                self.subviews.last?.removeFromSuperview()
+            }
+        }
+    }
+}
